ebeea48a374bccf0add2f0c359659d99dbc56668
Chris Lindbergh <chrislindbergh@gmail.com>
2020-03-12 21:52:57 -0600

    Add code to gather more data and place into text file

--- pc_mkgraph.py
+++ pc_mkgraph.py
@@ -13,36 +13,78 @@ can be applied with this cron entry:
 import time
 ### https://docs.python.org/3/library/sys.html
 import sys
+### https://docs.python.org/3/library/subprocess.html
+import subprocess
 
 ## VARIABLES
-### Toggle debugging if needed
-DEBUG = False
-### File to read information from
+### DEBUG: Toggle debugging if needed
+DEBUG = True
+### CPUCOUNTCMD: Program to find CPU count
+CPUCOUNTCMD = "/bin/grep -c -E '^CPU|^processor' </proc/cpuinfo"
+### LOADAVGCMD: Program to find load average
+LOADAVGCMD = "/bin/cat /proc/loadavg"
+### TIMEFORMAT: String used to format time
+TIMEFORMAT = "%Y-%m-%dT%H:%M:%S%z"
+### TXTFILE: File to read information from
 TXTFILE = "/media/kso/CIRCUITPY/hostinfo.txt"
-### Set output line to aid in troubleshooting
-OUTPUT = "EMPTY"
+### STDOUTCPU; OUTPUT; STDOUTLOAD: Set variables used later to aid in troubleshooting
+STDOUTCPU = OUTPUT = STDOUTLOAD = "EMPTY"
 
 ## CODE
+###  https://docs.python.org/3/tutorial/errors.html
+
+### Pull CPU count
+###  http://man7.org/linux/man-pages/man5/proc.5.html
+try:
+    STDOUTCPU = subprocess.run([CPUCOUNTCMD], shell=True, capture_output=True, text=True).stdout
+    STDOUTCPU = STDOUTCPU.strip()
+except OSError as err:
+    print("OS error: {0}".format(err))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+if DEBUG:
+    print("STDOUTCPU:", STDOUTCPU)
+
+### Pull load averages
+###  https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/4/html/Reference_Guide/s2-proc-loadavg.html
+###  "The first three columns measure CPU and IO utilization of the last one, five, and 10 minute periods. 
+###   The fourth column shows the number of currently running processes and the total number of processes. 
+###   The last column displays the last process ID used."
+try:
+    STDOUTLOAD = subprocess.run([LOADAVGCMD], shell=True, capture_output=True, text=True).stdout
+    STDOUTLOAD = STDOUTLOAD.strip()
+except OSError as err:
+    print("OS error: {0}".format(err))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+if DEBUG:
+    print("STDOUTLOAD:", STDOUTLOAD)
+
 ### Pull time and format to ISO 8601
 ###  https://www.tutorialspoint.com/python3/time_strftime.htm
 ###  https://en.wikipedia.org/wiki/ISO_8601
-LOCALTIME = time.strftime("%Y-%m-%dT%H:%M:%S%z", time.localtime(time.time()))
+LOCALTIME = time.strftime(TIMEFORMAT, time.localtime(time.time()))
 if DEBUG:
     print("LOCALTIME:", LOCALTIME)
+
 ### Write output to the CircuitPython drive
-###  https://docs.python.org/3/tutorial/errors.html
-OUTPUT = LOCALTIME
+OUTPUT = STDOUTCPU + " " + STDOUTLOAD + " " + LOCALTIME
 if DEBUG:
     print("OUTPUT:", OUTPUT)
 try:
     FILEOUT = open(TXTFILE, "w")
     FILEOUT.write(OUTPUT)
     FILEOUT.close()
+    if DEBUG:
+        print("File written:", TXTFILE)
 except OSError as err:
     print("OS error: {0}".format(err))
 except:
     print("Unexpected error:", sys.exc_info()[0])
     raise
+
 ### Let the user know we're done
 if DEBUG:
     print("*** Script Complete ***")
