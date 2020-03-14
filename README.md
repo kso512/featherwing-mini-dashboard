b27449ac2d1acf0f819c172399077e769848b5b8
Chris Lindbergh <chrislindbergh@gmail.com>
2020-03-13 22:35:32 -0600

    Improve comments and documentation

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The general idea behind this project is to be able to check on the status of a s
 Hardware for this project consists of:
 
 1. PC running Linux
-1. [Adafruit Feather STM32F405 Express](https://www.adafruit.com/product/4382) including stacking headers
+1. [Adafruit Feather STM32F405 Express including stacking headers](https://www.adafruit.com/product/4382)
 1. [Adafruit Mini Color TFT with Joystick FeatherWing](https://www.adafruit.com/product/3321)
 1. [I/O Crest Blank Tray Storage Box Drawer for 5.25" Bay Drive for Any PC Desktop Computer](https://amzn.com/B01LY3YDLN) (or equivalent)
 1. USB-C Cable
@@ -22,8 +22,7 @@ Software for this project consists of:
 
 1. Python script on the PC running Linux, gathering local information and saving it as a text file on the CircuitPython.
     1. Requires Python 3.8.2 or higher.
-    1. This script does not provide automation, as that can be applied with this cron entry:
-        > `* * * * * <repository path>/pc_mkgraph.py >/dev/null 2>&1`
+    1. This script does not provide automation as that can be applied with cron; see below.
 
 1. Python script on the Feather, converting the text file data into a graph and displaying the graph.
 
@@ -31,9 +30,23 @@ Software for this project consists of:
 
 ### Hardware Assembly
 
-After receiving all of the parts, assemble them so that the FeatherWing has male headers pointing down and the Feather has female headers pointing up.  Mate the two together, then plug it into your computer and follow the steps [here](https://learn.adafruit.com/welcome-to-circuitpython/installing-circuitpython) to install the latest version of CircuitPython.
+After receiving all of the parts, assemble them so that the FeatherWing has male headers pointing down and the Feather has female headers pointing up.  Mate the two together, then plug it into your computer and follow the steps [here](https://learn.adafruit.com/welcome-to-circuitpython/installing-circuitpython) to install the latest version of CircuitPython.  Connect the Feather to the PC running Linux with the USB-C cable, and note the path given to the mounted drive.
 
 Drill screw holes in the front of the drawer to match the mounting holes on the FeatherWing, then cut a hole in the drawer that matches the buttons and screen but leaves enough material for the screws to grip to.  The drawer may also need a notch in the back to allow a USB cable to pass through to the motherboard.
+
+### Software Installation and Configuration
+
+Clone the repository from GitHub.  Adjust the variables, especially TXTFILE so it matches the path given to the mounted drive.  Enable automation with cron using an entry such as this:
+> `* * * * * <repository path>/pc_mkgraph.py >/dev/null 2>&1`
+
+## References
+
+* [time — Time access and conversions](https://docs.python.org/3/library/time.html)
+* [sys — System-specific parameters and functions](https://docs.python.org/3/library/sys.html)
+* [subprocess — Subprocess management](https://docs.python.org/3/library/subprocess.html)
+* [Errors and Exceptions](https://docs.python.org/3/tutorial/errors.html)
+* [proc - process information pseudo-filesystem](http://man7.org/linux/man-pages/man5/proc.5.html)
+* [ISO 8601 Date and time format](https://www.iso.org/iso-8601-date-and-time-format.html)
 
 ## License
 
--- pc_mkgraph.py
+++ pc_mkgraph.py
@@ -6,37 +6,42 @@ and saving it as a text file on the CircuitPython. Requires Python
 3.8.2 or higher.  This script does not provide automation, as that 
 can be applied with this cron entry:
 `* * * * * <repository path>/pc_mkgraph.py >/dev/null 2>&1`
+
+References
+* https://docs.python.org/3/library/time.html
+* https://docs.python.org/3/library/sys.html
+* https://docs.python.org/3/library/subprocess.html
+* https://docs.python.org/3/tutorial/errors.html
+* http://man7.org/linux/man-pages/man5/proc.5.html
+* https://www.tutorialspoint.com/python3/time_strftime.htm
+* https://en.wikipedia.org/wiki/ISO_8601
 """
 
 ## LIBRARIES
-### https://docs.python.org/3/library/time.html
 import time
-### https://docs.python.org/3/library/sys.html
 import sys
-### https://docs.python.org/3/library/subprocess.html
 import subprocess
 
 ## VARIABLES
 ### DEBUG: Toggle debugging if needed
 DEBUG = True
+### TXTFILE: File to write information to
+TXTFILE = "/media/kso/CIRCUITPY/hostinfo.txt"
 ### CPUCOUNTCMD: Program to find CPU count
 CPUCOUNTCMD = "/bin/grep -c -E '^CPU|^processor' </proc/cpuinfo"
 ### LOADAVGCMD: Program to find load average
 LOADAVGCMD = "/bin/cat /proc/loadavg"
 ### TIMEFORMAT: String used to format time
 TIMEFORMAT = "%Y-%m-%dT%H:%M:%S%z"
-### TXTFILE: File to read information from
-TXTFILE = "/media/kso/CIRCUITPY/hostinfo.txt"
-### STDOUTCPU; OUTPUT; STDOUTLOAD: Set variables used later to aid in troubleshooting
+### STDOUTCPU; OUTPUT; STDOUTLOAD: Initialize variables to aid in
+###  troubleshooting
 STDOUTCPU = OUTPUT = STDOUTLOAD = "EMPTY"
 
 ## CODE
-###  https://docs.python.org/3/tutorial/errors.html
-
-### Pull CPU count
-###  http://man7.org/linux/man-pages/man5/proc.5.html
+### Pull CPU count; output should look like single number; Ex: `16`
 try:
-    STDOUTCPU = subprocess.run([CPUCOUNTCMD], shell=True, capture_output=True, text=True).stdout
+    STDOUTCPU = subprocess.run([CPUCOUNTCMD], shell=True,
+                        capture_output=True, text=True).stdout
     STDOUTCPU = STDOUTCPU.strip()
 except OSError as err:
     print("OS error: {0}".format(err))
@@ -46,13 +51,11 @@ except:
 if DEBUG:
     print("STDOUTCPU:", STDOUTCPU)
 
-### Pull load averages
-###  https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/4/html/Reference_Guide/s2-proc-loadavg.html
-###  "The first three columns measure CPU and IO utilization of the last one, five, and 10 minute periods. 
-###   The fourth column shows the number of currently running processes and the total number of processes. 
-###   The last column displays the last process ID used."
+### Pull load averages; output should look like 3 floats, a fraction, and a
+###  number; Ex: `8.32 7.89 7.92 12/2452 2670160`
 try:
-    STDOUTLOAD = subprocess.run([LOADAVGCMD], shell=True, capture_output=True, text=True).stdout
+    STDOUTLOAD = subprocess.run([LOADAVGCMD], shell=True,
+                        capture_output=True, text=True).stdout
     STDOUTLOAD = STDOUTLOAD.strip()
 except OSError as err:
     print("OS error: {0}".format(err))
@@ -62,9 +65,8 @@ except:
 if DEBUG:
     print("STDOUTLOAD:", STDOUTLOAD)
 
-### Pull time and format to ISO 8601
-###  https://www.tutorialspoint.com/python3/time_strftime.htm
-###  https://en.wikipedia.org/wiki/ISO_8601
+### Pull time and format to ISO 8601; output should look like a date and time 
+###  stamp with timezone; Ex: `2020-03-12T21:50:21-0600`
 LOCALTIME = time.strftime(TIMEFORMAT, time.localtime(time.time()))
 if DEBUG:
     print("LOCALTIME:", LOCALTIME)
